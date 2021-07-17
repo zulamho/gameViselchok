f81073ce2592fc7d75b623b3981f706efe971e96
Zulamho <amkhadov2014@mail.ru>
2021-07-17 12:41:17 +0300

    ff

---
+++ nameAnimals.js
@@ -0,0 +1,9 @@
+
+let animale = ['Кот','Рыба', 'Лемур', 'Комодский варан'];
+for(let i = 0 ; i < animale.length ; i++){
+  if(animale[i] === 'Кот' || animale[i] === 'Рыба' ||  animale[i] === 'Лемур' || animale[i] === 'Комодский варан'   ){
+    animale[i] += ' - Прекрасное животное'
+  }
+  
+}
+console.log(animale)
